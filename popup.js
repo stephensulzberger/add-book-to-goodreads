26519a693ba5af2e804169b615cbe39c09a18753
Stephen Sulzberger <stephen.sulzberger@gmail.com>
2020-10-24 11:59:28 -0700

    Update ISBN-10 regular expression to match unlimited whitespace before colon literal

--- popup.js
+++ popup.js
@@ -7,7 +7,7 @@ function(request, sender, sendResponse) {
     console.log('content type is ' + typeof(request));
     //console.log(request.content);
     //split into two regex patterns so it is a bit more clear what they actually do
-    isbnScan = request.content.match(/ISBN(-*1(?:(0)|3))?:?\s*?(97(8|9))?\d{9}(\d|X)/i);
+    isbnScan = request.content.match(/ISBN(-*1(?:(0)|3))?\s*?:?\s*?(97(8|9))?\d{9}(\d|X)/i);
     isbnScan2 = request.content.match(/ISBN(-*1(?:(0)|3))?:?\s[0-9]{1,}(\s|-)[0-9]{1,}(\s|-)[0-9]{4,}(\s|-)[0-9]{1,}(\s|-)(([0-9]|x){1,})*/i);
     console.log('isbn = ' + isbnScan);
     console.log('isbn2 = ' + isbnScan2);
